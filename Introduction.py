f28c3de81ec50ffc920a3d8a29a3948bdbbf165e
Wanschel <132244114+Wanschel@users.noreply.github.com>
2023-05-27 00:26:25 -0300

    Create Introduction.py

---
+++ Introduction.py
@@ -0,0 +1,13 @@
+while True:
+    num = int(input("Pick a number!"))
+  
+    if num == 50:
+        print("Bingo!")
+        break
+    elif num < 50:
+        print("Too low!")
+    else:
+        print("Too high!")
+    
+    
+    
