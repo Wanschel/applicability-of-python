1665d066a353d5bed2425c206a2c8f6828e14eda
Henoc Soares <nocsoares385@gmail.com>
2023-05-31 20:13:25 -0300

    Update Introduction.py
    Random number

--- Introduction.py
+++ Introduction.py
@@ -1,8 +1,10 @@
+import random
+
+rand = random.randint(0,100)
+
 while True:
     num = int(input("Pick a number between 0 and 100!"))
     
-    rand = random.randint(0,100)
-
     if num == rand:
         print("Bingo!")
         break
