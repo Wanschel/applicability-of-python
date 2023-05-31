1a147349de460eec12524c00ba15ea54af0323f6
Henoc Soares <nocsoares385@gmail.com>
2023-05-31 20:23:15 -0300

    Update Introduction.py
    DONE! SUCCESS \O/

--- Introduction.py
+++ Introduction.py
@@ -3,8 +3,14 @@ import random
 rand = random.randint(0,100)
 
 while True:
-    num = int(input("Pick a number between 0 and 100!\n"))
-    
+    num = int()
+
+    try:
+        num = int(float(input("Pick a number between 0 and 100!\n"))) #converts floating point input to integer
+    except ValueError: #gives the opportunity to try again (to input) if something not accepted is entered
+        print("Input invalid. Please enter a number: \n")
+        continue
+        
     if num == rand:
         print("Bingo!")
         break
